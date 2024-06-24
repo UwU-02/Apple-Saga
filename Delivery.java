6a72998901a88c0f2ad1220f591d41db4088693b
Tirah24 <limnur991@gmail.com>
2024-06-24 21:15:07 +0800

    Add files via upload

---
+++ Delivery.java
@@ -0,0 +1,28 @@
+package AppleSaga;
+
+public class Delivery {
+	
+	private String deliveryId;
+	private String deliveryDate;
+
+	public Delivery() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDeliveryId() {
+		return deliveryId;
+	}
+
+	public void setDeliveryId(String deliveryId) {
+		this.deliveryId = deliveryId;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+}
