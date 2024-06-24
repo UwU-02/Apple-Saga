2f0e16398e1a83cdbadeb4f379fd4837b1c17b0d
Jiachean <133480252+Jiachean@users.noreply.github.com>
2024-06-24 12:37:14 +0800

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -1,5 +1,28 @@
 package AppleSaga;
 
 public class Payment {
+	private int paymentId;
+	private double paymentAmount;
+	
+	public Payment() {
+		
+	}
+
+	public int getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public double getPaymentAmount() {
+		return paymentAmount;
+	}
+
+	public void setPaymentAmount(double paymentAmount) {
+		this.paymentAmount = paymentAmount;
+	}
+	
 	
 }
