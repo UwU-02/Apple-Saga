f86bc15f4c9333db2e22a79ccd4afd239ee4cc89
DICKSON LIM HAN PING <119713582+xiaodi0922@users.noreply.github.com>
2024-06-27 21:37:00 +0800

    Update CustomerController.java

--- src/com/apple/controller/CustomerController.java
+++ src/com/apple/controller/CustomerController.java
@@ -21,15 +21,16 @@ public class CustomerController extends Controller{
         }
     }
 
-    public Customer getCustomerDetailbyUsernamePassword(String username, String password) {
+    public Customer getCustomerDetailbyUsernamePassword(String email, String password) {
         Customer customer = null;
         try {
+        	customer = new Customer();
             String sql = "SELECT customerId, customerName, customerEmail, customerContact, customerAddress, customerPassword FROM CUSTOMER WHERE customerEmail = ? AND customerPassword = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, username);
+            ps.setString(1, email);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
+            while (rs.next()) {
                 customer = new Customer();
                 customer.setCustomerId(rs.getInt("customerId"));
                 customer.setCustomerName(rs.getString("customerName"));
