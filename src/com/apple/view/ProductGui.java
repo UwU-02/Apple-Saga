4cb588003ece6133276f9d276994d9bc20b5f944
lowxq <154233676+lowxq@users.noreply.github.com>
2024-06-27 21:30:59 +0800

    Update ProductGui.java

--- src/com/apple/view/ProductGui.java
+++ src/com/apple/view/ProductGui.java
@@ -35,8 +35,7 @@ public class ProductGui {
 
         filterBttn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame.dispose(); // Close the current frame
-                new FilterGui(null); // Open the filter page
+                new FilterGui(); // Open the filter page
             }
         });
 
@@ -48,9 +47,14 @@ public class ProductGui {
 
         Product product = new Product();
         ImageIcon imageIcon1 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage1 = new JLabel(imageIcon1);
-        productImage1.setBounds(92, 161, 150, 117);
+        JButton productImage1 = new JButton(imageIcon1);
+        productImage1.setBounds(92, 161, 149, 117);
         frame.getContentPane().add(productImage1);
+        productImage1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblIphone13 = new JLabel("Iphone 13");
         lblIphone13.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -58,19 +62,30 @@ public class ProductGui {
         frame.getContentPane().add(lblIphone13);
 
         ImageIcon imageIcon2 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage2 = new JLabel(imageIcon2);
+        JButton productImage2 = new JButton(imageIcon2);
         productImage2.setBounds(282, 161, 150, 117);
         frame.getContentPane().add(productImage2);
+        productImage2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
+        
         JLabel lblIphone14 = new JLabel("Iphone 14");
         lblIphone14.setFont(new Font("Serif", Font.PLAIN, 14));
         lblIphone14.setBounds(282, 288, 100, 21);
         frame.getContentPane().add(lblIphone14);
 
         ImageIcon imageIcon3 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage3 = new JLabel(imageIcon3);
+        JButton productImage3 = new JButton(imageIcon3);
         productImage3.setBounds(467, 161, 150, 117);
         frame.getContentPane().add(productImage3);
+        productImage3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblIphone15 = new JLabel("Iphone 15");
         lblIphone15.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -78,10 +93,15 @@ public class ProductGui {
         frame.getContentPane().add(lblIphone15);
 
         ImageIcon imageIcon4 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage4 = new JLabel(imageIcon4);
+        JButton productImage4 = new JButton(imageIcon4);
         productImage4.setIcon(imageIcon4);
         productImage4.setBounds(658, 161, 150, 117);
         frame.getContentPane().add(productImage4);
+        productImage4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblIpadAirinchi = new JLabel("Ipad Air 11-inch");
         lblIpadAirinchi.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -89,9 +109,14 @@ public class ProductGui {
         frame.getContentPane().add(lblIpadAirinchi);
 
         ImageIcon imageIcon5 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage5 = new JLabel(imageIcon5);
+        JButton productImage5 = new JButton(imageIcon5);
         productImage5.setBounds(91, 354, 150, 117);
         frame.getContentPane().add(productImage5);
+        productImage5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblIpadPro = new JLabel("Ipad Pro");
         lblIpadPro.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -99,9 +124,14 @@ public class ProductGui {
         frame.getContentPane().add(lblIpadPro);
 
         ImageIcon imageIcon6 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage6 = new JLabel(imageIcon6);
+        JButton productImage6 = new JButton(imageIcon6);
         productImage6.setBounds(282, 354, 150, 117);
         frame.getContentPane().add(productImage6);
+        productImage6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblAirpodsPro = new JLabel("Airpods Pro");
         lblAirpodsPro.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -109,9 +139,14 @@ public class ProductGui {
         frame.getContentPane().add(lblAirpodsPro);
 
         ImageIcon imageIcon7 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage7 = new JLabel(imageIcon7);
+        JButton productImage7 = new JButton(imageIcon7);
         productImage7.setBounds(467, 354, 150, 117);
         frame.getContentPane().add(productImage7);
+        productImage7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblAirpods = new JLabel("Airpods (3rd Gen)");
         lblAirpods.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -119,9 +154,14 @@ public class ProductGui {
         frame.getContentPane().add(lblAirpods);
 
         ImageIcon imageIcon8 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage8 = new JLabel(imageIcon8);
+        JButton productImage8 = new JButton(imageIcon8);
         productImage8.setBounds(658, 354, 150, 117);
         frame.getContentPane().add(productImage8);
+        productImage8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
 
         JLabel lblAppleWatchSeries9 = new JLabel("Apple Watch Series 9");
         lblAppleWatchSeries9.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -129,9 +169,14 @@ public class ProductGui {
         frame.getContentPane().add(lblAppleWatchSeries9);
         
         ImageIcon imageIcon9 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage9 = new JLabel(imageIcon9);
+        JButton productImage9 = new JButton(imageIcon9);
         productImage9.setBounds(880, 161, 150, 117);
         frame.getContentPane().add(productImage9);
+        productImage9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
         
         JLabel lblIpadProInch = new JLabel("Ipad Pro 12.9-inch");
         lblIpadProInch.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -145,9 +190,14 @@ public class ProductGui {
         frame.getContentPane().add(lblAppleWatchSe);
         
         ImageIcon imageIcon10 = new ImageIcon(product.getProductImageURL());
-        JLabel productImage10 = new JLabel(imageIcon10);
+        JButton productImage10 = new JButton(imageIcon10);
         productImage10.setBounds(880, 354, 150, 117);
         frame.getContentPane().add(productImage10);
+        productImage10.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              //  new ProductDetailGui(productId); // Open the product details page 
+            }
+        });
         
         
         JLabel lblPrice1 = new JLabel("RM 3199");
