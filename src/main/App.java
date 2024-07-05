9c23b5909921eb91c2c52bf82370f2040c061fbf
UwU-02 <133480252+UwU-02@users.noreply.github.com>
2024-07-05 17:33:36 +0800

    Add files via upload

---
+++ src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import com.apple.view.*;
+
+public class App {
+	public static void main(String args[]) throws Exception {
+    	
+        LoginPageGui frame = new LoginPageGui();
+        frame.setVisible(true);
+
+    }
+}
