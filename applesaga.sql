12797e02e3a3dc02d1974542b644bc2b0123abe2
watermelon <133223265+watermelonnini@users.noreply.github.com>
2024-07-06 22:03:23 +0800

    Update applesaga.sql

--- applesaga.sql
+++ applesaga.sql
@@ -171,14 +171,16 @@ INSERT INTO ORDER_LIST (productId, orderId, productQuantity) VALUES (2, 11, 1);
 
 CREATE TABLE CART_ITEM (
   cartId int(5),
-  productId int(5) REFERENCES PRODUCT (productId),
+  productId int(5),
   quantity int(2) DEFAULT 1,
-  PRIMARY KEY (cartId)
-)
+  PRIMARY KEY (cartId, productId),
+  FOREIGN KEY (productId) REFERENCES PRODUCT (productId)
+);
 
 CREATE TABLE CART (
   id int(5) UNIQUE AUTO_INCREMENT,
   customerId int(5) REFERENCES CUSTOMER (customerId),
   PRIMARY KEY (id)
-)
+);
+
 
