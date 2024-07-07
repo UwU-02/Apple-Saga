acb3f7ecddf53778c3a844a7f83caf482e20c6e9
UwU-02 <133480252+UwU-02@users.noreply.github.com>
2024-07-07 16:53:18 +0800

    Update App.java

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,14 @@
 package main;
 
+import javax.swing.JFrame;
+
 import com.apple.view.*;
 
 public class App {
-	public static void main(String args[]) throws Exception {
-    	
+	public static void main(String args[]) throws Exception {	
+		
         LoginPageGui frame = new LoginPageGui();
         frame.setVisible(true);
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
